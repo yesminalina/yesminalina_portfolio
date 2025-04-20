0f5f19caef6cb62d656f87d248958ec4c9529406
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-04-20 01:10:26 -0400

    fix: add animation

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,4 +1,5 @@
 import { Element } from 'react-scroll'
+import StackBox from './StackBox'
 import Html from './Icons/Html'
 import Css from './Icons/Css'
 import Bootstrap from './Icons/Bootstrap'
@@ -59,7 +60,7 @@ const Stack = () => {
       return (
         <div className='flex flex-col justify-center align-middle me-2 mb-2' key={lang}>
           <i className='self-center'><IconComponent width={56} height={56} /></i>
-          <p className='self-center text-xs'>{lang}</p>
+          <p className='self-center text-xs pt-1'>{lang}</p>
         </div>
       )
     })
@@ -68,33 +69,33 @@ const Stack = () => {
   return (
     <Element name='stack' className='py-28 bg-yellow_pale min-h-svh'>
       <div className='flex flex-col justify-around align-middle md:max-w-[1100px] mx-auto'>
-        <h2 className='self-center text-4xl text-olive uppercase font-bold mb-16'>My Stack <span className='text-pink text-4xl'>.</span></h2>
-        <div className='flex justify-around'>
-          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl hover:shadow-inner bg-gradient-to-br from-mint'>
-            <h3 className='uppercase text-slate-500 font-semibold self-center mb-4'>Frontend</h3>
+        <h2 className='self-center text-4xl text-olive uppercase font-bold mb-24'>My Stack <span className='text-pink text-4xl'>.</span></h2>
+        <StackBox className='flex justify-around'>
+          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl bg-gradient-to-br from-mint '>
+            <h3 className='uppercase text-olive font-semibold self-center mb-4'>Frontend</h3>
             <div className='flex flex-wrap justify-center'>
               {renderStack(frontend)}
             </div>
           </article>
-          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl hover:shadow-inner bg-gradient-to-br from-mint'>
-            <h3 className='uppercase text-slate-500 font-semibold self-center mb-4'>Backend</h3>
+          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl bg-gradient-to-br from-mint '>
+            <h3 className='uppercase text-olive font-semibold self-center mb-4'>Backend</h3>
             <div className='flex flex-wrap justify-center'>
               {renderStack(backend)}
             </div>
           </article>
-          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl hover:shadow-inner bg-gradient-to-br from-mint'>
-            <h3 className='uppercase text-slate-500 font-semibold self-center mb-4'>Tools</h3>
+          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl bg-gradient-to-br from-mint '>
+            <h3 className='uppercase text-olive font-semibold self-center mb-4'>Tools</h3>
             <div className='flex flex-wrap justify-center'>
               {renderStack(tools)}
             </div>
           </article>
-          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl hover:shadow-inner bg-gradient-to-br from-mint'>
-            <h3 className='uppercase text-slate-500 font-semibold self-center mb-4'>Data Analysis</h3>
+          <article className='flex flex-col w-1/4 me-4 p-4 rounded-lg shadow-xl bg-gradient-to-br from-mint '>
+            <h3 className='uppercase text-olive font-semibold self-center mb-4'>Data Analysis</h3>
             <div className='flex flex-wrap justify-center'>
               {renderStack(dataAnalysis)}
             </div>
           </article>
-        </div>
+        </StackBox>
       </div>
     </Element>
   )
