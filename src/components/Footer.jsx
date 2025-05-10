da2571cdbc6cdc6d7f4576a29872bcab4707ee0b
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-05-10 14:16:46 -0400

    Feat: fix Footer for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Footer = () => {
   const robotIcon = <FontAwesomeIcon icon={faRobot} size='lg' />
   return (
-    <footer className='flex justify-center text-olive p-4 uppercase text-xs font-medium border-t'>
-      <p className='m-0 self-center'>Built with Reactjs and Tailwind CSS by Yesminalina - 2025 </p>
-      <i className='px-2 self-center text-pink'>{robotIcon}</i>
+    <footer className='flex justify-center text-olive p-4 text-xs font-medium w-full'>
+      <article className='flex justify-center w-full justify-start border-t pt-2'>
+        <p className='m-0 self-center'>Built with Reactjs and Tailwind CSS by Yesminalina - 2025 </p>
+        <i className='sm:px-2 self-center text-pink'>{robotIcon}</i>
+      </article>
     </footer>
   )
 }
