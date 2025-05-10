9c7dd67c4dc8cdc6ba72e833c80ddb238d85da11
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-05-10 13:28:07 -0400

    Feat: fix header for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,13 +11,17 @@ const Header = () => {
   return (
     <Element name='about' className='flex justify-center align-bottom md:max-w-[1100px] min-h-svh mx-auto sm:pt-28 text-olive'>
       <div className='flex flex-col sm:flex sm:flex-row sm:justify-between md:self-end sm:h-[540px] bg-none sm:bg-hero-img bg-contain bg-bottom bg-no-repeat'>
-        <section className='flex justify-center w-full min-h-dvh bg-hero-img bg-contain bg-bottom bg-no-repeat sm:bg-none px-6'>
-          <div className='flex flex-col w-full md:w-8/12 pt-32 sm:pt-0'>
+        <section className='flex sm:w-8/12 min-h-dvh bg-hero-img bg-contain bg-bottom bg-no-repeat sm:bg-none px-6'>
+          <div className='flex flex-col w-full sm:w-8/12 pt-24 sm:pt-0'>
             <p className='text-xl text-pink'>Hey there, my name is</p>
             <h1 className='text-start font-semibold text-6xl sm:text-8xl max-w-36'>Yesmin Pizarro<span className='text-pink'>.</span></h1>
             <article className='sm:hidden'>
               <h2 className='text-xl md:text-3xl uppercase font-semibold'>JS Fullstack Developer</h2>
               <p className='text-pink font-semibold my-4'>Aplying and elevating my skills to create new things.</p>
+              <article className='flex justify-start'>
+                <a href='https://www.linkedin.com/in/yesminpizarro/'>{linkedinIcon}</a>
+                <a href='https://github.com/yesminalina'>{githubIcon}</a>
+              </article>
             </article>
             <Link className='hidden sm:block mt-24 ms-36 alagn-bottom text-pink animate-bounce cursor-pointer' smooth duration={500} to='projects'>{arrowDown}</Link>
           </div>
