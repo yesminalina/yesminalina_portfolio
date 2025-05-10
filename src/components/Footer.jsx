b22bf1243931400dae2ada2d48bda2c89109b403
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-05-10 14:20:42 -0400

    Fix: fix footer for mobile section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className='flex justify-center text-olive p-4 text-xs font-medium w-full'>
       <article className='flex justify-center w-full border-t pt-2'>
         <p className='m-0 self-center'>Built with Reactjs and Tailwind CSS by Yesminalina - 2025 </p>
-        <i className='sm:px-2 self-center text-pink'>{robotIcon}</i>
+        <i className='px-2 self-center text-pink'>{robotIcon}</i>
       </article>
     </footer>
   )
