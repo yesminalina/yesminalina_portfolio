aa30abc725222441a328bbc0aafe62aa73ffde03
yesmin pizarro <yesmin.pizarro@gmail.com>
2025-05-10 20:08:17 -0400

    Fix: fix Stack Section animation

--- src/components/StackBox.jsx
+++ src/components/StackBox.jsx
@@ -16,8 +16,10 @@ const StackBox = ({children}) => {
   }, [])
 
   return (
-    <article ref={ArticleRef} className={`transition-all duration-500 ease-in ${visible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-40'} flex flex-col sm:w-1/4 mb-4 sm:me-4 p-4 rounded-lg shadow-xl bg-pink/20 backdrop-blur-xl border-pink/30 border`}>
-      {children}
+    <article ref={ArticleRef} className={`transition-all transform-3d duration-1000 ease-in-out ${visible ? 'opacity-100 [transform:rotateY(180deg)]' : 'opacity-0'} sm:w-1/4 mb-4 sm:me-4 p-4 rounded-lg shadow-xl bg-pink/20 backdrop-blur-xl border-pink/30 border`}>
+      <div className='[transform:rotateY(180deg)] flex flex-col justify-center'>
+        {children}
+      </div>
     </article>
   )
 }
